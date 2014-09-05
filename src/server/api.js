7cd2fad4a719f5c042b84b775c933287cc57e712
Adam Sax <adam@spotlabs.com>
2014-09-04 17:19:56 -0700

    okay first pass of the api's are ready, time to start building out the front end.

--- src/server/api.js
+++ src/server/api.js
@@ -1,24 +1,77 @@
 var express = require('express');
+var _ = require("underscore");
+var uuid = require('node-uuid');
+var fs = require('fs');
+var path = require('path');
+
+var jsonPath = path.resolve(__dirname, "../../data/lists.json");
+var lists =  require(jsonPath);
+
+function save() {
+    fs.writeFile(jsonPath, JSON.stringify(lists, null, '\t'), function (err) {
+        if (err) throw err;
+        console.log('Data json successfully saved');
+    });
+}
+
 var api = express.Router();
 
+// gets all lists
 api.get('/list', function (req, res) {
-    res.end("here are all the lists");
+    res.setHeader('Content-Type', 'application/json');
+    res.send(lists);
 });
 
+// creates a new list
 api.post('/list', function (req, res) {
-    res.end("You just created a new list");
+
+    // grab the list coming in from the request
+    var list = req.body;
+    // create a new id
+    list.id = uuid.v4();
+    // save it to memory
+    lists.push(list)
+
+    // flush what's in memory to disk
+    save();
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(list);
 });
 
-api.put('/list/', function (req, res) {
-    res.end("You just edited an existing list");
+// edits a list
+api.put('/list/:id/', function (req, res) {
+    // search for the list by id
+    var list = _.findWhere(lists, {id: req.params.id});
+
+    // if we can't find it throw a 404
+    res.setHeader('Content-Type', 'application/json');
+    if (_.isUndefined(list)) {
+        res.status(404).send({"error": "cannot find list " + req.params.id})
+        return;
+    }
+
+    // update the list in memory with the body
+    _.extend(list, req.body)
+
+    // flush the lists to disk
+    save();
+
+    // respond with the updated list
+    res.send(list);
 });
 
+// get a specific list
 api.get('/list/:id', function (req, res) {
-    res.end("here's your specified list");
-});
+    var list = _.findWhere(lists, {id: req.params.id});
 
-api.put('/list/:id', function (req, res) {
-    res.end("you just edited a specific list");
+    if (_.isUndefined(list)) {
+        res.status(404).send({"error": "cannot find list " + req.params.id})
+        return;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(list);
 });
 
 module.exports = api;
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../public'));
 app.use('/api', require('./api'));
 
